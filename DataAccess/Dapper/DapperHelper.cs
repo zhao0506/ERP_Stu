bb316fb8753736f655c97acb7d91a4136e94de78
3352736063@qq.com <3352736063@qq.com>
2020-06-18 09:10:34 +0800

    测试效果

--- DataAccess/Dapper/DapperHelper.cs
+++ DataAccess/Dapper/DapperHelper.cs
@@ -1,13 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
 
 namespace DataAccess.Dapper
 {
     /// <summary>
     /// Dapper访问数据库类
     /// </summary>
-    public class DapperHelper
+    public static class DapperHelper<T> 
+       
     {
+
+        static string strconn = "Data Source=192.168.1.126;Initial Catalog=ERPDB;Persist Security Info=True;User ID=sa";
+
+        /// <summary>
+        /// 增删改
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static int CRD(string sql)
+        {
+
+            using (SqlConnection conn=new SqlConnection(strconn))
+            {
+
+
+                return conn.Execute(sql);
+
+            }
+        
+        }
+        /// <summary>
+        /// 显示查询
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+
+        public static List<T> GetAll(string sql)
+        {
+
+            using (SqlConnection conn=new SqlConnection(strconn))
+            {
+                return conn.Query<T>(sql).ToList();
+            }
+        
+        }
+
     }
 }
---
+++ DataAccess/ERP_ProjectDal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+using System.Data.SqlClient;
+
+namespace DataAccess
+{
+  public  class ERP_ProjectDal
+    {
+    }
+}
